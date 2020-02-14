d24388af807b9eab2d21eaa6e27e5e9c2d426626
arthur josi <arthur.josi@outlook.fr>
2020-02-14 10:31:31 +0100

    Sending_path_node done

--- src/ROS_projet_pkg/scripts/sending_path_node.py
+++ src/ROS_projet_pkg/scripts/sending_path_node.py
@@ -3,14 +3,7 @@ import rospy
 from heapq import *
 from math import sqrt
 from geometry_msgs.msg import Pose 
-
-
-
-
-
-
-
-
+from simple_controller.msg import path 
 
 # Initialize ROS::node
 rospy.init_node('Coordinate', anonymous=True)
@@ -23,21 +16,36 @@ Coord_et_adjacence = {
 	"3" : [[4,0],[2]]	
 } 
 
-def Graph_manager(data) : 
-	pose_msg = Pose ()
+posx =0
+posy =0
+graph = {}
+
+def robot_pos(data) : 
+	global posx
+	global posy
+	print("ROBOT POS RECEIVED")
 	posx = data.position.x
 	posy = data.position.y
+
+def Graph_manager(data) : 
+	global posx
+	global posy
+	global graph	
+	print("DESKTOP NUMBER RECEIVED" )
+	path_to_pub = path()
+	aim_desktop = int(data.position.x)
 	graph, n = Graph_creation(Coord_et_adjacence, posx, posy)
+	print(graph)
 	# Recuperation du path sous forme de liste avec le numero des points par lesquels passer
-	path = dijkstra(str(n), Point_to_reach , voisins)[1]
+	paths = dijkstra(str(n), str(aim_desktop) , voisins)[1]
 	# Creation de la liste qui contiendra les coordonnees des points du path
 	coord_path = []
-	for k in range(1, len(path)) : 
-		coord_path.append(Coord_et_adjacence[path[k]])
-	#Pour le moment on envoie directement la liste des coordonnees des points a parcourir dans la carte
-	pose_msg.position.x = coord_path
-	#pose_msg.position.y = 0 
-	pose_pub.publish(pose_msg)	
+	for k in range(1,len(paths)) : 
+		coord_path.append(Coord_et_adjacence[paths[k]][0])
+	path_to_pub.path= coord_path
+
+	print(path_to_pub.path)
+	path_pub.publish(path_to_pub)	
 	
 def Graph_creation(Coord, posx, posy):
 	Graph_list = []
@@ -54,7 +62,7 @@ def Graph_creation(Coord, posx, posy):
 			Graph_list[i][1].append(    ( sqrt((Coord[str(i)][0][0]-Coord[point_to_compare][0][0])**2 + (Coord[point_to_compare][0][1]-Coord[str(j)][0][1])**2),str(point_to_compare) )          )
 	#Ajout des distance entre le robot et les differents autres points
 	for i in range(n) :
-		Graph_list[n][1].append(    ( sqrt((Coord[str(i)][0][0]-posx)**2 + (Coord[point_to_compare][0][1]-posy),str(n)))      )
+		Graph_list[n][1].append(    ( sqrt((Coord[str(i)][0][0]-posx)**2 + (Coord[point_to_compare][0][1]-posy)),str(i)  )      )
 			
 	return(dict(Graph_list), n)
 
@@ -90,24 +98,31 @@ def dijkstra (s, t, voisins):
                 heappush(suivants, (dy, y))
                 p[y] = x
 
-    path = [t]
+    paths = [t]
     x = t
     while x != s:
         x = p[x]
-        path.insert(0, x)
+        paths.insert(0, x)
 
-    return d[t], path
+    return d[t], paths
 
 def voisins (s):
+    global graph
     return graph[s]
 
 
 
 if __name__ == '__main__':
 	print("Start sending path node")
-	get_robot_pose = rospy.Subscriber('/robot_pose', Pose, Graph_manager) 
+	
+	#Get the robot position in map
+	rospy.Subscriber('/robot_pose', Pose, robot_pos) 
+	
+	#Get the aim point in data.position.x
+	rospy.Subscriber('/aim_desktop', Pose, Graph_manager)
 
-	pose_pub = rospy.Publisher('/path', Pose, queue_size= 10)
+	#Publisher for different pose to reach with the robot 
+	path_pub = rospy.Publisher('/path', path, queue_size= 1)
 	
 	# spin() simply keeps python from exiting until this node is stopped
 	rospy.spin()
